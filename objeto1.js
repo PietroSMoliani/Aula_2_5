b9c1dea6e864fa09a7fac39c3fab91379382d441
PietroSMoliani <pietromoliani@colegiopaulodetarso.com.br>
2025-06-23 16:00:45 +0000

    final

---
+++ objeto1.js
@@ -0,0 +1,10 @@
+const touro = {
+    marca: "Touro",
+    cor: "preto",
+    altura: "1,60m",
+    raça: "Angus",
+    mudarCanal: function() {
+        console.log("Canal alterado");
+    }
+};
+touro.mudarCanal();
